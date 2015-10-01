7aa68bbe5c6e736fde250be20b01535c27f67a98
Alex Rivadeneira <alexrivs@gmail.com>
2015-09-30 20:01:50 -0700

    cleanup for heroku

--- db/migrate/20150926204249_add_column_to_games.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToGames < ActiveRecord::Migration
-	def up
-		add_column "games", "total", :integer, :default => 0
-	end
-
-	def down
-		remove_column "games", "total", :integer
-	end
-end

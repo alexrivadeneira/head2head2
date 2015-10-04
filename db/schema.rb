7e257b0310986216cab545bb27e90c487657c936
Alex Rivadeneira <alexrivs@gmail.com>
2015-10-04 11:46:54 -0700

    basically working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004154939) do
+ActiveRecord::Schema.define(version: 20151004183555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 20151004154939) do
     t.integer  "score",      default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.integer  "comment_id"
+    t.integer  "guess_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "concepts", force: :cascade do |t|
